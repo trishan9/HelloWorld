d7b633d73f0d5e3fa42fd8231d4facd66872801e
Anantashayana <anantashayana.hegde@gmail.com>
2023-10-02 08:40:54 +0530

    Added Scala Hello World

---
+++ Scala/HelloWorld.scala
@@ -0,0 +1,10 @@
+// Define an object named "HelloWorld."
+object HelloWorld {
+
+  // Define the main method, the program's entry point.
+  def main(args: Array[String]): Unit = {
+    
+    // Print "Hello, World!" to the console.
+    println("Hello, World!")
+  }
+}
