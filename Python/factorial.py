9ded344db67af83587e4c4df31d7c0ea0dd8af35
BAKA-470 <vivekjoshi2267735@gmail.com>
2023-10-29 18:26:41 +0530

    Added program to check a palindrome number and factorial of a number

---
+++ Python/factorial.py
@@ -0,0 +1,8 @@
+def fact(num):
+    if num == 0 or num == 1:
+        return 1
+
+    return num * fact(num - 1)
+
+sol = fact(6)
+print(sol)
---
+++ Python/palindromNumber.py
@@ -0,0 +1,13 @@
+def pal(num):
+    temp = num
+    ans = 0
+    while(temp > 0):
+        rem = temp % 10
+        temp = temp//10
+        ans = ans * 10 + rem
+    if ans == num:
+        return True
+    else:
+        return False
+
+print(pal(12111))
