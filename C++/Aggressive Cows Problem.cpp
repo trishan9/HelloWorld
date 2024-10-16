0d626a27bb1c3ee6f64ca4b218202c9b2e657e8e
Vishnu Gupta <guptavishnu2711@gmail.com>
2024-10-16 18:02:53 +0530

    Aggressive Cows Problem

--- C++/Aggressive Cows Problem.cpp
+++ C++/Aggressive Cows Problem.cpp
@@ -1,54 +1,38 @@
-#include<iostream>
-#include<algorithm>
-using namespace std;
-
-bool isPossible(int arr[], int k, int mid, int n){
-
-int cowCount = 1;
-int lastPos = arr[0];
-
-for(int i =0; i<n; i++){
-
-    if(arr[i]-lastPos >= mid){
-        cowCount++;
-        if(cowCount==k){
-            return true;
+bool isposible(vector<int> &stalls, int k,int mid){
+    int cow = 1;
+    int last = stalls[0];
+    for(int i=0 ; i<stalls.size();i++){
+        if(stalls[i]-last>=mid){
+         cow++;
+         if (cow == k) {
+              return true;
+            }
+            last = stalls[i];
         }
-        lastPos = arr[i];
     }
+    return false;
 }
-return false;
-}
-
-
-
-int aggressiveCow(int arr[], int k, int n){
-    sort(arr, arr + n);
+int aggressiveCows(vector<int> &stalls, int k)
+{
+    //    Write your code here.
+    sort(stalls.begin(),stalls.end());
     int s = 0;
     int maxi = -1;
-    for(int i = 0; i<n; i++){
-        maxi = max(maxi, arr[i]);
+    for(int i=0 ; i< stalls.size() ;i++){
+        maxi = max(maxi , stalls[i]);
     }
     int e = maxi;
     int ans = -1;
     int mid = s + (e-s)/2;
-
-    while(s<=e){
-        if(isPossible(arr, k, mid, n)){
-            ans = mid;
-            s = mid + 1;
+    while(e>=s){
+        if(isposible(stalls,k,mid)){
+            ans=mid;
+            s = mid+1;
         }
         else{
-            e = mid -1;
+            e = mid-1;
         }
         mid = s + (e-s)/2;
     }
     return ans;
-}
-
-int main(){
-
-int arr[5] = {4,2,3,1,5};
-cout<< aggressiveCow(arr, 2, 5);
-
 }
